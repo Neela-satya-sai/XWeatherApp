8e71e34222c6a0d8c5fb9c39e014e8649222ba8c
neelasatyasai <neelasatyasai9.3@gmail.com>
2025-06-01 18:16:45 +0530

    done with functionality

--- xweatherapp/src/WeatherApp/WeatherApp.jsx
+++ xweatherapp/src/WeatherApp/WeatherApp.jsx
@@ -1,12 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Box, TextField, Button, Card, Typography,  CardContent, Grid} from "@mui/material";
+import axios from "axios";
+import {
+  Box,
+  TextField,
+  Button,
+  Card,
+  Typography,
+  CardContent,
+  Grid,
+  Form,
+} from "@mui/material";
 import styles from "./WeatherApp.module.css";
 
-function SearchBar() {
+function SearchBar({ setUserText, makeApiCall }) {
   const [input, setInput] = useState("");
 
-  function handleInput(e){
-     setInput(e.target.value);
+  function handleInput(e) {
+    let value = e.target.value;
+    setInput(value);
+    setUserText(value);
   }
 
   return (
@@ -27,54 +39,107 @@ function SearchBar() {
         }}
         onChange={handleInput}
       ></TextField>
-      <Button className={styles.button} >Search</Button>
+      <Button className={styles.button} onClick={makeApiCall}>
+        Search
+      </Button>
     </Box>
   );
 }
 
-function WeatherCard({title, data}) {
-  return <Box>
-
+function WeatherCard({ title, data = "", unit="" }) {
+  return (
+    <Box className={"weather-card"}>
       <Card width={"25vw"}>
-         <CardContent sx={{ height: '100%' }}>
-              <Typography variant="h5" component="div">
-                {title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                hfllkd;ks
-              </Typography>
-            </CardContent>
+        <CardContent sx={{ height: "100%" }}>
+          <Typography variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {data}{data? unit : null}
+          </Typography>
+        </CardContent>
       </Card>
-  </Box>;
+    </Box>
+  );
 }
 
-function WeatherCards(){
-  return(
-    <Box display={"flex"} flexWrap={"wrap"} columnGap={5} justifyContent={"center"}  >
-
-      <WeatherCard title={"Temperature"}></WeatherCard>
-       <WeatherCard title={"Humidity"}></WeatherCard>
-        <WeatherCard title={"Condition"}></WeatherCard>
-         <WeatherCard title={"Wind Speed"}></WeatherCard>
+function WeatherCards({ weatherData }) {
+  return (
+    <Box
+      display={"flex"}
+      flexWrap={"wrap"}
+      columnGap={5}
+      justifyContent={"center"}
+      className={"weather-cards"}
+    >
+      <WeatherCard
+        title={"Temperature"}
+        data={weatherData.temperature}
+        unit={"°C"}
+      ></WeatherCard>
+      <WeatherCard title={"Humidity"} data={weatherData.humidity}  unit={"%"}></WeatherCard>
+      <WeatherCard
+        title={"Condition"}
+        data={weatherData.condition}
+      ></WeatherCard>
+      <WeatherCard
+        title={"Wind Speed"}
+        data={weatherData["wind speed"]}  unit={"k/hr"}
+      ></WeatherCard>
     </Box>
-
   );
 }
 
 function WeatherApp() {
-
   const [usertext, setUserText] = useState("");
-  
-  useEffect(()=>{
+  const [weatherData, setWeatherData] = useState({});
+  const [fetching, setFetching] = useState(false);
+  // const [makeApiCall, setMakrApiCall] = useState(false);
 
-    
-  },[]);
+  // useEffect(() => {
+  //   // console.log(data);
+  // }, [makeApiCall]);
+
+  function makeApiCall() {
+    setFetching(true);
+    console.log(usertext);
+    let data = axios
+      .get("https://api.weatherapi.com/v1/current.json", {
+        params: { key: "ad016ae906bb4220acf154736253105", q: usertext },
+      })
+      .then((data) => {
+        // console.log("Weather Data:", data);
+        let res = data.data;
+        console.log(res);
+        let weatherData = {
+          temperature: res.current.temp_c,
+          humidity: res.current.wind_kph,
+          condition: res.current.condition.text,
+          "wind speed": res.current.wind_kph,
+        };
+        setWeatherData(weatherData);
+      })
+      .catch((error) => {
+        alert("Failed to fetch weather data");
+      })
+      .finally(() => {
+        setFetching(false);
+      });
+  
+  }
 
   return (
     <>
       <div>WeatherApp</div>
-      <SearchBar ></SearchBar>
-      <WeatherCards></WeatherCards>
+      <SearchBar
+        setUserText={setUserText}
+        makeApiCall={makeApiCall}
+      ></SearchBar>
+      {fetching ? (
+        <p>Loading data…</p>
+      ) : (
+        <WeatherCards weatherData={weatherData}></WeatherCards>
+      )}
     </>
   );
 }
